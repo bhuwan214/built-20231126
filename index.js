49b0ec8867564ad01de6b22c180b3d133725d50b
void-the-droid <bhuwan626282@gmail.com>
2023-12-02 21:29:57 +0545

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 gsap.from(".profile-box2",{
-    x:-700,
+    x:-720,
     duration:2,
-    delay:0.5
-    
+    delay:0.5,
+    scale:2
+})
+
+gsap.from("h2",{
+    x:-900,
+    duration:2,
+    delay:0.5,
+})
+
+gsap.from("h3",{
+    x:-900,
+    duration:2,
+    delay:0.5,
+})
+
+gsap.from(".line",{
+    x:900,
+    duration:2,
+    delay:0.5,
+    rotate:360,
+    scale:1.5
 })
