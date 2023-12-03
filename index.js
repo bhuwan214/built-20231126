e1fc11e85d4c81815f72457ddc3223da91ae5323
void-the-droid <bhuwan626282@gmail.com>
2023-12-03 09:40:41 +0545

    don't know how long

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 gsap.from(".profile-box2",{
-    x:-720,
+    x:-400,
     duration:2,
     delay:0.5,
-    scale:2
+    scale:2,
+    opacity:0,
 })
 
 gsap.from("h2",{
-    x:-900,
+    x:800,
     duration:2,
     delay:0.5,
+    opacity:0,
 })
 
 gsap.from("h3",{
-    x:-900,
+    x:800,
     duration:2,
     delay:0.5,
+    opacity:0,
 })
 
 gsap.from(".line",{
@@ -22,5 +25,6 @@ gsap.from(".line",{
     duration:2,
     delay:0.5,
     rotate:360,
-    scale:1.5
+    scale:1.5,
 })
+
